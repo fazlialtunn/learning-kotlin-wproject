02ce103bbcbbaed189d729a12a0637ed184a3b1c
Fazlı Altun <altunfazl@gmail.com>
2023-05-16 13:22:39 +0300

    first commit

---
+++ app/src/main/java/com/example/a2004projecttry6/LearnCard5Activity.kt
@@ -0,0 +1,11 @@
+package com.example.a2004projecttry6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LearnCard5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learn_card5)
+    }
+}
---
+++ app/src/main/java/com/example/a2004projecttry6/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.a2004projecttry6
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.cardview.widget.CardView
+import com.example.a2004projecttry6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var D1 : CardView ? = null
+    private var D2 : CardView ? = null
+    private var D3 : CardView ? = null
+    private var D4 : CardView ? = null
+    private var D5 : CardView ? = null
+    private var D6 : CardView ? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        D1= findViewById(R.id.learncard1)
+        D2= findViewById(R.id.learncard2)
+        D3= findViewById(R.id.learncard3)
+        D4= findViewById(R.id.learncard4)
+        D5= findViewById(R.id.learncard5)
+        D6= findViewById(R.id.learncard6)
+
+        D1?.setOnClickListener(this)
+        D2?.setOnClickListener(this)
+        D3?.setOnClickListener(this)
+        D4?.setOnClickListener(this)
+        D5?.setOnClickListener(this)
+        D6?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        val i:Intent = when (v.id) {
+            R.id.learncard1 -> Intent(this, LearnCard1Activity::class.java)
+            R.id.learncard2 -> Intent(this, LearnCard2Activity::class.java)
+            R.id.learncard3 -> Intent(this, LearnCard3Activity::class.java)
+            R.id.learncard4 -> Intent(this, LearnCard4Activity::class.java)
+            R.id.learncard5 -> Intent(this, LearnCard5Activity::class.java)
+            R.id.learncard6 -> Intent(this, LearnCard6Activity::class.java)
+            else -> throw IllegalArgumentException("Unhandled view ID: ${v.id}")
+        }
+        startActivity(i)
+    }
+}
