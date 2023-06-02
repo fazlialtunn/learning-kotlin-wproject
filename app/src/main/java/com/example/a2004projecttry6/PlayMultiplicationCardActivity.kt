38d60b12f66ce7e80a348b411b687781020aadf5
Fazlı Altun <altunfazl@gmail.com>
2023-06-03 00:15:56 +0300

    second commit

---
+++ app/src/main/java/com/example/a2004projecttry6/PlayMultiplicationCardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2004projecttry6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayMultiplicationCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_multiplication_card)
+    }
+}
---
+++ app/src/main/java/com/example/a2004projecttry6/PlayRememberNumsBackwardCardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2004projecttry6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayRememberNumsBackwardCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_remember_nums_backward_card)
+    }
+}
