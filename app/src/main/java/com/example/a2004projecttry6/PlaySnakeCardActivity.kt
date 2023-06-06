e92847d22d847c2db02cdd38cb0ee5adfdeb566a
Fazlı Altun <altunfazl@gmail.com>
2023-06-06 17:16:51 +0300

    Added: Turkish language has been added.

---
+++ app/src/main/java/com/example/a2004projecttry6/PlaySnakeCardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2004projecttry6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaySnakeCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_snake_card)
+    }
+}
